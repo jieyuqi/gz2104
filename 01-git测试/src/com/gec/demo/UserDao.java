6b6a7d33f45cd4da129e2847ba8b4fac477e81c8
xiaoming <xiaoming@qq.com>
2021-05-11 10:23:27 +0800

    李四新建了一个branch分支

--- 01-git测试/src/com/gec/demo/UserDao.java
+++ 01-git测试/src/com/gec/demo/UserDao.java
@@ -4,28 +4,7 @@ public interface UserDao {
 	
 void addUser();
 	
-	//push�ύ
-	void addUser2();
-	
-
-	//push�ύ
-		void addUser3();
-		
-		
-	//李四提交的方法
-		void aa();
-		
-		//zhangsan提交的aaa
-		void zhangsan_aa();
-		
-		//zhangsan提交的
-		void zhangsan();
-		
-		//zhangsan提交的
-		void zhangsan2();
-		//lisi提交的
-		void lisi();
-		
+	void test_branch();
 	
 
 }
